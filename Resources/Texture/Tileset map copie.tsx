a4447d40bac52a065c171ecb47379d5c01c93d2b
Sh6pa <71717569+Sh6pa@users.noreply.github.com>
2022-01-24 17:30:08 +0100

    MainMap & door setup

---
+++ Resources/Texture/Tileset map copie.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Tileset map copie" tilewidth="32" tileheight="32" tilecount="357" columns="21">
+ <image source="../Map - Copie.png" width="700" height="564"/>
+</tileset>
